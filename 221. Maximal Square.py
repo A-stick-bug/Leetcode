a9a89a70933b9cdecbf57b7be4f3410a4c77c295
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-19 17:30:58 -0400

    fixed index error

--- 221. Maximal Square.py
+++ 221. Maximal Square.py
@@ -8,7 +8,7 @@ def maximalSquare(matrix: List[List[str]]) -> int:
 
     for r in range(1, rows + 1):
         for c in range(1, cols + 1):
-            if matrix[r][c] == "1":  # matrix and dp have 0 and 1 indexing respectively
+            if matrix[r-1][c-1] == "1":  # matrix and dp have 0 and 1 indexing respectively
                 dp[r][c] = min(dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1]) + 1
                 res = max(res, dp[r][c])
     return res ** 2  # squared because we are looking for area
