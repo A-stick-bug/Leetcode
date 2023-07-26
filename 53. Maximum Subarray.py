45670062ae1fc8af9232ca88b05cb21cc45acec4
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-26 10:49:57 -0400

    simplified code

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,7 +1,7 @@
 def maxSubArray(nums) -> int:
     res = -float('inf')
     current = 0
-    for i in range(len(nums)):
-        current = max(nums[i], current+nums[i])  # if current is negative, reset to 0
+    for n in nums:
+        current = max(n, current + n)  # if current is negative, reset to 0
         res = max(res, current)  # update max value
     return res
