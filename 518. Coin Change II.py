aa0bfb52fab03852192c6f5a5fbfd57df1dddbb5
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-12 10:38:11 -0400

    added dp solution

--- 518. Coin Change II.py
+++ 518. Coin Change II.py
@@ -16,4 +16,15 @@ def change(amount: int, coins: List[int]) -> int:
     return solve(amount, 0)
 
 
+def bottom_up_method(amount: int, coins: List[int]) -> int:
+    dp = [0] * (amount + 1)
+    dp[0] = 1
+
+    for i in reversed(range(len(coins))):
+        for j in range(coins[i], amount + 1):
+            dp[j] += dp[j - coins[i]]
+
+    return dp[amount]
+
+
 print(change(amount=5, coins=[1, 2, 5]))
