f4eb48c4a237342af2a13450ada94e1284ad3c86
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-30 21:01:42 -0400

    rectangle in histogram problem(s)

--- 84. Largest Rectangle in Histogram.py
+++ 84. Largest Rectangle in Histogram.py
@@ -1,4 +1,5 @@
-# increasing monotonic stack
+# strictly increasing monotonic stack
+# previous smaller problem
 
 from typing import List
 
@@ -9,11 +10,14 @@ def largestRectangleArea(heights: List[int]) -> int:
     res = 0
     for i in range(len(heights)):
 
-        # keep stack in increasing order
+        # keep stack in increasing order (strict)
         while stack and heights[stack[-1]] >= heights[i]:
             height = heights[stack.pop()]
-            width = i if not stack else i - stack[-1] - 1  # minus one because it starts from the previous element
+
+            # if the stack is empty, then the current height can extend all the way to the left
+            width = i if not stack else i - stack[-1] - 1  # minus one because it is the width between 2 points
             res = max(res, height * width)  # check the maximum area we can obtain with current height
+
         stack.append(i)
 
     return res
---
+++ 85. Maximal Rectangle.py
@@ -0,0 +1,45 @@
+"""
+for each row, we use the largest rectangle in histogram method to find the maximum area in O(M)
+time complexity is O(MN) because there are N rows
+
+to convert the matrix to a histogram, we increment by 1 if the value is 1 and set to 0 if it is 0
+because a 0 will prevent a rectangle being formed from the bottom
+"""
+
+from typing import List
+
+
+def largest_histogram(heights):
+    heights.append(0)  # this will make sure we check remaining elements in stack after iterating all heights
+    stack = []
+    res = 0
+    for i in range(len(heights)):
+
+        # keep stack in increasing order (strict)
+        while stack and heights[stack[-1]] >= heights[i]:
+            height = heights[stack.pop()]
+
+            # if the stack is empty, then the current height can extend all the way to the left
+            width = i if not stack else i - stack[-1] - 1  # minus one because it is the width between 2 points
+            res = max(res, height * width)  # check the maximum area we can obtain with current height
+
+        stack.append(i)
+    return res
+
+
+def maximalRectangle(matrix: List[List[str]]) -> int:
+    ROWS, COLS = len(matrix), len(matrix[0])
+    histogram = [0] * COLS  # build a histogram and update on every row
+    max_area = 0
+
+    for row in matrix:
+        for c in range(COLS):  # update histogram
+            histogram[c] = (histogram[c] + 1) if row[c] == "1" else 0
+
+        # get max area using the current row and every row above it
+        max_area = max(max_area, largest_histogram(histogram))
+
+    return max_area
+
+
+print(maximalRectangle([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]))
