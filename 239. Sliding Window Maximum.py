d8ed2edd141a91022cbde2636f4bcdb865cad44e
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-09-04 21:55:37 -0400

    added solution

--- 239. Sliding Window Maximum.py
+++ 239. Sliding Window Maximum.py
@@ -1,28 +1,52 @@
+# we want the MAXIMUM element in each window, so we use a decreasing monotonic queue
+
 from collections import deque
 
 
 def maxSlidingWindow(nums, k):
+    n = len(nums)
     window = deque()  # keep track of window indices
     result = []
-    left = right = 0
+    left = 0
 
-    while right < len(nums):
-        # remove until the last element in window ISN'T less than current element
+    for right in range(n):
+        # decreasing queue: remove until the last element in window ISN'T less than current element
         while window and window[-1] < nums[right]:
             window.pop()
         window.append(nums[right])
 
+        # only check if window is full, otherwise keep incrementing right
         window_size = right - left + 1
-        if window_size < k:  # if less than window size, keep adding elements (while also keeping only the maximum)
-            right += 1
-
-        elif window_size == k:  # window is full
+        if window_size == k:
             result.append(window[0])  # window[0] is always the largest as smaller ones were removed already
+
             if window[0] == nums[left]:  # remove max element as it is no longer in the window
                 window.popleft()
 
             # shift window
             left += 1
-            right += 1
 
     return result
+
+
+# similar solution that stores indices in the queue instead of the actual value
+def solution2(nums, k):
+    n = len(nums)
+    window = deque()
+    res = []
+    left = 0
+
+    for right in range(n):
+        while window and nums[window[-1]] < nums[right]:
+            window.pop()
+        window.append(right)
+
+        size = right - left + 1
+        if size == k:
+            res.append(nums[window[0]])
+
+            if window[0] == left:
+                window.popleft()
+            left += 1
+
+    return res
