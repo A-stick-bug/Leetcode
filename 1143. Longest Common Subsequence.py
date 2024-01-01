e578678c59ef7421b2ea23bf31e45cd22178574b
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-12-31 21:30:11 -0500

    added iterative solution

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -1,3 +1,18 @@
+# using tabulation/bottom up DP
+def longestCommonSubsequence_iterative(text1: str, text2: str) -> int:
+    n, m = len(text1), len(text2)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    for i in reversed(range(n)):  # fill in reverse as we start with the smaller sub problems
+        for j in reversed(range(m)):
+            if text1[i] == text2[j]:  # equal letters, increase the longest answer by 1
+                dp[i][j] = dp[i + 1][j + 1] + 1
+            else:  # either exclude a letter in the first or second work
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+    return dp[0][0]
+
+
 from functools import cache
 
 
