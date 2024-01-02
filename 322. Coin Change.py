5b98a60f14fac6b3762bdb5367a92aa54671f233
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-01-02 17:24:23 -0500

    added dp solution

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -1,6 +1,23 @@
-# BFS solution dp could use used as well
+# https://leetcode.com/problems/coin-change/
+# dynamic programming solution
+def coinChange_DP(coins, amount: int) -> int:
+    dp = [0] * (amount + 1)  # base case is 0 since we can just not have any coin
 
+    for i in range(1, amount + 1):
+        res = float('inf')
+        for c in coins:
+            if i - c >= 0:  # reachable from a previous state, +1 since we are using an extra coin
+                res = min(res, dp[i - c] + 1)
+        dp[i] = res  # choose the previous state that uses the least coins
+
+    return dp[amount] if dp[amount] != float('inf') else -1
+
+
+print(coinChange_DP(coins=[1, 2, 5], amount=11))
+
+# BFS solution
 from collections import deque
+
 
 def coinChange(coins, amount: int) -> int:
     q = deque()
@@ -17,6 +34,3 @@ def coinChange(coins, amount: int) -> int:
                 q.append((amt - coin, n_coins + 1))
 
     return -1
-
-
-print(coinChange(coins=[1,5], amount=11))
