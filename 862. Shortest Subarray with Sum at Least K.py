79b1a0137181f4de072aa252a8434cb0602b5d29
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-01-01 22:28:36 -0500

    phew, quite a hard mono q question

---
+++ 862. Shortest Subarray with Sum at Least K.py
@@ -0,0 +1,29 @@
+# ⭐ hard question, worth reviewing
+# https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/description/
+# monotonic queue with prefix sum
+# for each index, we want to remove as many prefix elements as possible to reduce the subarray's size
+# however, we must also ensure that the remaining elements still sum up to less than k
+
+from collections import deque
+
+
+def shortestSubarray(nums: list[int], k: int) -> int:
+    q = deque([(0, -1)])  # (prefix sum up to index (inclusive), index)
+    cur = 0
+    res = float('inf')
+
+    for right in range(len(nums)):
+        cur += nums[right]
+        while q and cur - q[0][0] >= k:  # remove extra elements so the remaining is still less than k
+            res = min(res, right - q.popleft()[1])
+
+        # remove > elements since we might as well remove everything up to the current if it will result in a GREATER
+        # sum, note the >= because even if we remove the same amount, it is more optimal to remove more elements
+        while q and q[-1][0] >= cur:
+            q.pop()
+        q.append((cur, right))  # add the current prefix as an option to remove
+
+    return res if res != float('inf') else -1
+
+
+print(shortestSubarray(nums = [1], k = 1))
