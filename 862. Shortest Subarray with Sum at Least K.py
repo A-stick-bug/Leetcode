957d9519f8d639b5946a9f75a739037144dfde39
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-01-17 10:51:18 -0500

    file organizing + review

--- 862. Shortest Subarray with Sum at Least K.py
+++ 862. Shortest Subarray with Sum at Least K.py
@@ -1,8 +1,10 @@
 # ⭐ hard question, worth reviewing
 # https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/description/
 # monotonic queue with prefix sum
+#
 # for each index, we want to remove as many prefix elements as possible to reduce the subarray's size
 # however, we must also ensure that the remaining elements still sum up to more than k
+# similar to first greater element (also uses deque)
 
 from collections import deque
 
